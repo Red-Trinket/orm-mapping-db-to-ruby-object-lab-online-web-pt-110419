473025394292e99f2658d174b4f99a08926ea703
Matthew Jones <matts3371@gmail.com>
2020-01-29 02:56:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,8 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     sql = <<-SQL 
-      
+      SELECT * FROM 
+      students
     # return a new instance of the Student class
   end
   
