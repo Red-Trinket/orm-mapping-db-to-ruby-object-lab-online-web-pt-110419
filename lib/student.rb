332a89c2346036e7c762ae1861bb04b7dbcdd04b
Matthew Jones <matts3371@gmail.com>
2020-01-29 03:13:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,8 +111,8 @@ class Student
     
     DB[:conn].execute(sql).map do |row| 
       self.new_from_db(row)
+      new_student
     end 
-    new_student
   end 
   
   def self.all_students_in_grade_X(num)
